89b83f2b6cd8e0e9d6d5076440808572a1661a47
Sebastian Nagel <sebastian.nagel@ncoding.at>
2023-12-01 23:09:33 +0100

    Solve part 2 in a super hacky way

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 advent_of_code::solution!(1);
 
 pub fn calibration_value(line: &str) -> u32 {
@@ -19,8 +21,58 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(res)
 }
 
+pub fn calibration_value2(line: &str) -> u32 {
+    let numbers = HashMap::from([
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+
+    let mut first = None;
+    let mut first_ix = line.len();
+    for (key, value) in numbers.iter() {
+        match line.find(key) {
+            Some(i) if i <= first_ix => {
+                first_ix = i;
+                first = Some(value);
+            }
+            _ => {}
+        }
+        continue;
+    }
+    let mut last = None;
+    let mut last_ix = 0;
+    for (key, value) in numbers.iter() {
+        match line.rfind(key) {
+            Some(i) if i >= last_ix => {
+                last_ix = i;
+                last = Some(value);
+            }
+            _ => {}
+        }
+        continue;
+    }
+    first.unwrap() * 10 + last.unwrap()
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let res = input.lines().map(|x| calibration_value2(x)).sum();
+    Some(res)
 }
 
 #[cfg(test)]
@@ -37,6 +89,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(281));
     }
 }
