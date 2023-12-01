49c9c099e9da95a89be3564df57cde45b8e49d19
Sebastian Nagel <sebastian.nagel@ncoding.at>
2023-12-01 08:55:56 +0100

    Day 1 part 1 digit parsing

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,22 @@
 advent_of_code::solution!(1);
 
+pub fn calibration_value(line: &str) -> u32 {
+    let mut cs = line.chars();
+
+    let first = cs.find(|x| x.is_ascii_digit()).and_then(|c| c.to_digit(10));
+    let last = cs
+        .rfind(|x| x.is_ascii_digit())
+        .and_then(|c| c.to_digit(10));
+
+    let a = first.or(last);
+    let b = last.or(first);
+
+    a.unwrap() * 10 + b.unwrap()
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let res = input.lines().map(calibration_value).sum();
+    Some(res)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -15,7 +30,8 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        println!("{:?}", result);
+        assert_eq!(result, Some(142));
     }
 
     #[test]
